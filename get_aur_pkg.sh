823f4385aea391fc38b131313cce8e605119cd34
Eliott Lavier <67601109+eliottlv@users.noreply.github.com>
2023-06-23 19:17:07 +0200

    Add more packages

--- get_aur_pkg.sh
+++ get_aur_pkg.sh
@@ -43,5 +43,33 @@ repo-add ../../airootfs/var/lib/pacman/aur-repo.db.tar.gz sha3sum*.pkg.*
 cp ./sha3sum*.pkg.* ../../airootfs/var/lib/pacman/
 cd ..; rm -rf ./sha3sum
 
+## Dirbuster
+git clone https://aur.archlinux.org/dirbuster.git
+cd dirbuster; chmod -R 777 .; sudo -u archbuild makepkg -sr --noconfirm
+repo-add ../../airootfs/var/lib/pacman/aur-repo.db.tar.gz dirbuster*.pkg.*
+cp ./dirbuster*.pkg.* ../../airootfs/var/lib/pacman/
+cd ..; rm -rf ./dirbuster
+
+## Dirbuster wordlists
+git clone https://aur.archlinux.org/dirbuster-wordlists.git
+cd dirbuster-wordlists; chmod -R 777 .; sudo -u archbuild makepkg -sr --noconfirm
+repo-add ../../airootfs/var/lib/pacman/aur-repo.db.tar.gz dirbuster-wordlists*.pkg.*
+cp ./dirbuster-wordlists*.pkg.* ../../airootfs/var/lib/pacman/
+cd ..; rm -rf ./dirbuster-wordlists
+
+## Age
+git clone https://aur.archlinux.org/age.git
+cd age; chmod -R 777 .; sudo -u archbuild makepkg -sr --noconfirm
+repo-add ../../airootfs/var/lib/pacman/aur-repo.db.tar.gz age*.pkg.*
+cp ./age*.pkg.* ../../airootfs/var/lib/pacman/
+cd ..; rm -rf ./age
+
+## yt-dlp
+git clone https://aur.archlinux.org/yt-dlp.git
+cd yt-dlp; chmod -R 777 .; sudo -u archbuild makepkg -sr --noconfirm
+repo-add ../../airootfs/var/lib/pacman/aur-repo.db.tar.gz yt-dlp*.pkg.*
+cp ./yt-dlp*.pkg.* ../../airootfs/var/lib/pacman/
+cd ..; rm -rf ./yt-dlp
+
 cd ..
 rm -rf ./airootfs/var/lib/pacman/*.old ./aur
